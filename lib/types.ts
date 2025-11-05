465e6561020df9afc9ddf49c5f3c27a3a8b28f60
Leonardo Nascimento <leonct13@gmail.com>
2025-11-05 17:44:40 -0300

    Resolvendo problema de build para prod

--- lib/types.ts
+++ lib/types.ts
@@ -1,25 +1,27 @@
-export enum UserType {
-    CONSULTANT = 'CONSULTANT',
-    CLIENT = 'CLIENT'
-  }
-  
-  export interface User {
-    id: string
-    name: string
-    email: string
-    phone: string
-    cpf: string
-    age: number
-    address: string
-    cep: string
-    state: string
-    complement?: string
-    type: UserType
-    consultantId?: string
-    consultant?: User
-    clients?: User[]
-    createdAt: Date
-    updatedAt: Date
-  }
-  
-  export type Consultant = Pick<User, 'id' | 'name' | 'email'>
+export const UserType = {
+  CONSULTANT: 'CONSULTANT',
+  CLIENT: 'CLIENT'
+} as const
+
+export type UserType = typeof UserType[keyof typeof UserType]
+
+export interface User {
+  id: string
+  name: string
+  email: string
+  phone: string
+  cpf: string
+  age: number
+  address: string
+  cep: string
+  state: string
+  complement?: string
+  type: UserType
+  consultantId?: string
+  consultant?: User
+  clients?: User[]
+  createdAt: Date
+  updatedAt: Date
+}
+
+export type Consultant = Pick<User, 'id' | 'name' | 'email'>
